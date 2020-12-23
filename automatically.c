835e6261456557f00ddc16ec26f4421727a7a443
Matvey <70555513+LUX23@users.noreply.github.com>
2020-12-23 10:23:56 +0700

    calculation part
    Размер и значения массива уже заданы

---
+++ automatically.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int aut()
+{
+    int arr[3][3] = {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9},
+                    };
+    printf("\tМатрица:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("\t%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    double m1, m2, m3, k;
+    m1 = (arr[1][0] + arr[2][0]) / 3.00;
+    m2 = (arr[0][1] + arr[2][1]) / 3.00;
+    m3 = (arr[0][2] + arr[1][2]) / 3.00;
+    printf("\n\tВектор:");
+    double vec[3] = {m1, m2, m3};
+    k = vec[0];
+    for (int j = 0; j < 3; j++)
+    {
+        printf("\t%.2f ", vec[j]);
+        if (k > vec[j])
+            k = vec[j];
+    }
+    printf("\n\tМинимальный элемент вектора = ");
+    printf("%.2f", k);
+    return 0;
+}
