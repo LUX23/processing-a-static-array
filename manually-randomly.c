3e28510a75e0df208347ce082b0b3a70c02fecd3
Matvey <70555513+LUX23@users.noreply.github.com>
2020-12-23 10:35:09 +0700

    Add files via upload

---
+++ manually-randomly.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int man_r()
+{
+    int n;
+    printf("\tСоздание матрицы размером n*n. Введите значение n: ");
+    scanf("%d", &n);
+    int arr[n][n];
+    printf("\n\tМатрица:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = rand()%100;
+            printf("\t%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    double k, m,
+           a = 0;
+    double vec[n];
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if(i != j)
+            a += arr[i][j];
+        }
+        m = a / n;
+        vec[j] = m;
+        a = 0;
+    }
+    printf("\n\tВектор:");
+    k = vec[0];
+    for (int j = 0; j < n; j++)
+    {
+        printf("\t%.2f ", vec[j]);
+        if (k > vec[j])
+        k = vec[j];
+    }
+    printf("\n\tМинимальный элемент вектора = ");
+    printf("%.2f", k);
+    return(0);
+}
