40afaaae586a1abb12e577dcbd7a3242fd1fb09b
Matvey <70555513+LUX23@users.noreply.github.com>
2020-12-28 12:39:51 +0700

    calculate part
    Размер и значения массива уже заданы

--- automatically.c
+++ automatically.c
@@ -1,7 +1,5 @@
 // Размер и значения массива уже заданы
 #include <stdio.h>
-#include <stdlib.h>
-#include <locale.h>
 
 int aut()
 {
@@ -10,7 +8,7 @@ int aut()
                         {4, 5, 6},
                         {7, 8, 9},
                     };
-    
+
     printf("\tМатрица:\n");
     for (int i = 0; i < 3; i++)
     {
@@ -20,26 +18,26 @@ int aut()
         }
         printf("\n");
     }
-    
-    double m1, m2, m3, k;
-    m1 = (arr[1][0] + arr[2][0]) / 3.00;
-    m2 = (arr[0][1] + arr[2][1]) / 3.00;
-    m3 = (arr[0][2] + arr[1][2]) / 3.00;
+
+    float m1, m2, m3, k;
+    m1 = (arr[1][0] + arr[2][0]) / 2.00;
+    m2 = (arr[0][1] + arr[2][1]) / 2.00;
+    m3 = (arr[0][2] + arr[1][2]) / 2.00;
     // среднее арифметическое столбца
-    
+
     printf("\n\tВектор:");
-    double vec[3] = {m1, m2, m3};
+    float vec[3] = {m1, m2, m3};
     k = vec[0];
-    
+
     for (int j = 0; j < 3; j++)
     {
         printf("\t%.2f ", vec[j]);
         if (k > vec[j]) // поиск минимального элемента вектора
             k = vec[j];
     }
-    
+
     printf("\n\tМинимальный элемент вектора = ");
     printf("%.2f", k);
-    
+
     return 0;
 }
